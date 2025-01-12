c1e9935013e0c8ec6809941a4683634a9ea1afa1
Bailly Rurangirwa <rubailly@gmail.com>
2025-01-12 13:59:45 +0200

    style: Format School class for consistent indentation and spacing

--- src/main/java/rw/gov/mineduc/qamis/integration/model/School.java
+++ src/main/java/rw/gov/mineduc/qamis/integration/model/School.java
@@ -7,154 +7,154 @@ import javax.persistence.Id;
 @Entity
 public class School {
 
-    @Id
-    private Integer schoolCode;
+  @Id private Integer schoolCode;
 
-    @Column(nullable = false)
-    private String schoolName;
+  @Column(nullable = false)
+  private String schoolName;
 
-    @Column(nullable = false)
-    private String province;
+  @Column(nullable = false)
+  private String province;
 
-    @Column(nullable = false)
-    private String district;
+  @Column(nullable = false)
+  private String district;
 
-    @Column(nullable = false)
-    private String sector;
+  @Column(nullable = false)
+  private String sector;
 
-    @Column(nullable = true)
-    private String cell;
+  @Column(nullable = true)
+  private String cell;
 
-    @Column(nullable = true)
-    private String village;
+  @Column(nullable = true)
+  private String village;
 
-    @Column(nullable = true)
-    private String schoolStatus;
+  @Column(nullable = true)
+  private String schoolStatus;
 
-    @Column(nullable = true)
-    private String schoolOwner;
+  @Column(nullable = true)
+  private String schoolOwner;
 
-    private Double latitude;
-    private Double longitude;
+  private Double latitude;
+  private Double longitude;
 
-    @Column(nullable = true)
-    private String day;
+  @Column(nullable = true)
+  private String day;
 
-    @Column(nullable = true)
-    private String boarding;
+  @Column(nullable = true)
+  private String boarding;
 
-    @Column(nullable = true)
-    private String schoolEmail;
+  @Column(nullable = true)
+  private String schoolEmail;
 
-    // Getters and Setters
-    public Integer getSchoolCode() {
-        return schoolCode;
-    }
+  // Getters and Setters
+  public Integer getSchoolCode() {
+    return schoolCode;
+  }
 
-    public void setSchoolCode(Integer schoolCode) {
-        this.schoolCode = schoolCode;
-    }
+  public void setSchoolCode(Integer schoolCode) {
+    this.schoolCode = schoolCode;
+  }
 
-    public String getSchoolName() {
-        return schoolName;
-    }
+  public String getSchoolName() {
+    return schoolName;
+  }
 
-    public void setSchoolName(String schoolName) {
-        this.schoolName = schoolName;
-    }
+  public void setSchoolName(String schoolName) {
+    this.schoolName = schoolName;
+  }
 
-    public String getProvince() {
-        return province;
-    }
+  public String getProvince() {
+    return province;
+  }
 
-    public void setProvince(String province) {
-        this.province = province;
-    }
+  public void setProvince(String province) {
+    this.province = province;
+  }
 
-    public String getDistrict() {
-        return district;
-    }
+  public String getDistrict() {
+    return district;
+  }
 
-    public void setDistrict(String district) {
-        this.district = district;
-    }
+  public void setDistrict(String district) {
+    this.district = district;
+  }
 
-    public String getSector() {
-        return sector;
-    }
+  public String getSector() {
+    return sector;
+  }
 
-    public void setSector(String sector) {
-        this.sector = sector;
-    }
+  public void setSector(String sector) {
+    this.sector = sector;
+  }
 
-    public String getCell() {
-        return cell;
-    }
+  public String getCell() {
+    return cell;
+  }
 
-    public void setCell(String cell) {
-        this.cell = cell;
-    }
+  public void setCell(String cell) {
+    this.cell = cell;
+  }
 
-    public String getVillage() {
-        return village;
-    }
+  public String getVillage() {
+    return village;
+  }
 
-    public void setVillage(String village) {
-        this.village = village;
-    }
+  public void setVillage(String village) {
+    this.village = village;
+  }
 
-    public String getSchoolStatus() {
-        return schoolStatus;
-    }
+  public String getSchoolStatus() {
+    return schoolStatus;
+  }
 
-    public void setSchoolStatus(String schoolStatus) {
-        this.schoolStatus = schoolStatus;
-    }
+  public void setSchoolStatus(String schoolStatus) {
+    this.schoolStatus = schoolStatus;
+  }
 
-    public String getSchoolOwner() {
-        return schoolOwner;
-    }
+  public String getSchoolOwner() {
+    return schoolOwner;
+  }
 
-    public void setSchoolOwner(String schoolOwner) {
-        this.schoolOwner = schoolOwner;
-    }
+  public void setSchoolOwner(String schoolOwner) {
+    this.schoolOwner = schoolOwner;
+  }
 
+  public String getSchoolEmail() {
+    return schoolEmail;
+  }
 
-    public String getSchoolEmail() {
-        return schoolEmail;
-    }
-    public void setSchoolEmail(String schoolEmail) {
-        this.schoolEmail = schoolEmail;
-    }
+  public void setSchoolEmail(String schoolEmail) {
+    this.schoolEmail = schoolEmail;
+  }
 
-    public Double getLatitude() {
-        return latitude;
-    }
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
+  public Double getLatitude() {
+    return latitude;
+  }
 
-    public Double getLongitude() {
-        return longitude;
-    }
+  public void setLatitude(Double latitude) {
+    this.latitude = latitude;
+  }
 
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
+  public Double getLongitude() {
+    return longitude;
+  }
 
-    public String getDay() {
-        return day;
-    }
+  public void setLongitude(Double longitude) {
+    this.longitude = longitude;
+  }
 
-    public void setDay(String day) {
-        this.day = day;
-    }
+  public String getDay() {
+    return day;
+  }
 
-    public String getBoarding() {
-        return boarding;
-    }
+  public void setDay(String day) {
+    this.day = day;
+  }
 
-    public void setBoarding(String boarding) {
-        this.boarding = boarding;
-    }
+  public String getBoarding() {
+    return boarding;
+  }
+
+  public void setBoarding(String boarding) {
+    this.boarding = boarding;
+  }
 }
